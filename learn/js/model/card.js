d93a8464d73c391d5587e8df43ad9ec49ca98688
landerqi <landerqi@gmail.com>
2014-06-04 15:26:03 +0800

    一堆

--- learn/js/model/card.js
+++ learn/js/model/card.js
@@ -5,10 +5,18 @@
     'use strict';
 
     function Card(){
-
+        this.isFaceUp = false;
+        this.isUnplayable = false;
     }
     Card.prototype = {
 
+        contents: function(v){
+            if (v === undefined){
+                return this._contents;
+            } else{
+                return this._contents = v;
+            }
+        }
     };
 
     function exports(){

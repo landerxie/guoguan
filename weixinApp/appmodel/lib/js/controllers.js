4401138c0ae940647b33e1425bb4db17cdb88923
landerqi <landerqi@gmail.com>
2014-06-19 16:35:20 +0800

    小修改

--- weixinApp/appmodel/lib/js/controllers.js
+++ weixinApp/appmodel/lib/js/controllers.js
@@ -2,135 +2,64 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
-
-    var score,result;
+    $scope.score = 0;
 
     $scope.action = "index";
 
-    $scope.item = [
+    $scope.result = {
+        title:"这是测试结果的title",
+        result:"析出八万项雨",
+        analysis: "费用是是这是相的你，半中不中费用是是这是相的你，半中不中费用是是这是相的你，半中不中费用是是这是相的你，半中不中。"
+    }
+
+    $scope.wxapp = [
         {
-            "name":"1、你要送一幅字给朋友，你会选什么字：",
-            "choose": [{
-                "img":"",
-                "text":"A、天道酬勤",
-                "score":1
-            },
-                {
-                    "img":"",
-                    "text":"B、地道酬善",
-                    "score":1
-                },
-                {
-                    "img":"",
-                    "text":"C、商道酬信",
-                    "score":1
-                },
-                {
-                    "img":"",
-                    "text":"D、业道酬精",
-                    "score":1
-                },
-            ]
+            title:"心理测试一" ,
+            count:"112334" ,
+            href:"http://www.guoguan.com" ,
+            img:"themes/images/2_03.jpg"
         },
         {
-            "name":"2、你认为美是？",
-            "choose": [{
-                "img":"",
-                "text":"A. 20以下",
-                "score":1
-            },
-                {
-                    "img":"",
-                    "text":"B. 21-30",
-                    "score":2
-                },
-                {
-                    "img":"",
-                    "text":"C. 31-40",
-                    "score":3
-                },
-                {
-                    "img":"",
-                    "text":"D. 41-50",
-                    "score":3
-                },
-                {
-                    "img":"",
-                    "text":"E. 50以上",
-                    "score":2
-                }
-            ]
+            title:"心理测试一",
+            count:"112334" ,
+            href:"http://www.guoguan.com" ,
+            img:"themes/images/2_04.jpg"
+        }
+    ]
+
+    $scope.postCluster = [
+        {
+            title:"集合贴一" ,
+            detail:"这是一个集合贴" ,
+            href:"http://www.guoguan.com" ,
+            img:"themes/images/2_03.jpg"
         },
         {
-            "name":"3、你送礼主要会考虑：",
-            "choose": [{
-                "img":"",
-                "text":"A、实用、有品质",
-                "score":3
-            },
-                {
-                    "img":"",
-                    "text":"B、独特、有创意",
-                    "score":3
-                },
-                {
-                    "img":"",
-                    "text":"C、大气、有内涵",
-                    "score":3
-                },
-                {
-                    "img":"",
-                    "text":"D、贵重、有分量",
-                    "score":3
-                }
-            ]
+            title:"集合贴二",
+            detail:"这是一个集合贴" ,
+            href:"http://www.guoguan.com" ,
+            img:"themes/images/2_04.jpg"
         }
-    ];
+    ]
 
-   /* var browser={
-        versions:function(){
-            var u = navigator.userAgent, app = navigator.appVersion;
-            return {         //移动终端浏览器版本信息
-                trident: u.indexOf('Trident') > -1, //IE内核
-                presto: u.indexOf('Presto') > -1, //opera内核
-                webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-                mobile: !!u.match(/AppleWebKit.*Mobile.*//*), //是否为移动终端
-                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
-                iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器
-                iPad: u.indexOf('iPad') > -1, //是否iPad
-                webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
-            };
-        }(),
-        language:(navigator.browserLanguage || navigator.language).toLowerCase()
-    }*/
-    /*document.writeln("语言版本: "+browser.language);
-     document.writeln(" 是否为移动终端: "+browser.versions.mobile);
-     document.writeln(" ios终端: "+browser.versions.ios);
-     document.writeln(" android终端: "+browser.versions.android);
-     document.writeln(" 是否为iPhone: "+browser.versions.iPhone);
-     document.writeln(" 是否iPad: "+browser.versions.iPad);
-     document.writeln(navigator.userAgent);*/
+    $("ul.item li > a").click(function(){
+        var scoreTmp = $(this).data("cent");
+        $scope.score += scoreTmp;
+    })
 
-    /*function is_weixin(){
-        var ua = navigator.userAgent.toLowerCase();
-        if(ua.match(/MicroMessenger/i)=="micromessenger") {
-            return true;
-        } else {
-            return false;
+    $scope.showResult = function (){
+        if($scope.score < 10){
+            alert("小于10");
+            window.location.hash = "result";
+        }else
+        if($scope.score > 10 && $scope.score < 20){
+            alert("小于20");
+            window.location.hash = "result";
+        }else{
+            alert("大于20");
+            window.location.hash = "result";
         }
-    }*/
-    /*function clientBrowseCheck(){
-        if(is_weixin()){
-            //微信浏览器
-            $("#share-to-sina-button").styles("display","inline-block");
-            $("#share-to-sina-button-pc").styles("display","none");
-            return
-        }
-    }*/
-
-
+    }
 
 });
 
